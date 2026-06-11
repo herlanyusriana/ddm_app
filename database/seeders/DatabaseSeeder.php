df99ef4d87b3cbe75255fee1cf610e61f22add28
herlanyusriana <navylan717@gmail.com>
2026-06-11 20:48:59 +0700

    Seed production processes only

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -2,7 +2,6 @@
 
 namespace Database\Seeders;
 
-use App\Models\User;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 
@@ -16,10 +15,5 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call(ProcessSeeder::class);
-
-        User::factory()->create([
-            'name' => 'Admin',
-            'email' => 'admin@example.com',
-        ]);
     }
 }
